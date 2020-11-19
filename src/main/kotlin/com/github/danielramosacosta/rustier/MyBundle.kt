d02c2d35a8b3791c88d315e069c6ae47d8117c9f
GitHub Action <action@github.com>
2020-11-19 16:00:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/danielramosacosta/rustier/MyBundle.kt
+++ src/main/kotlin/com/github/danielramosacosta/rustier/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.danielramosacosta.rustier
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/danielramosacosta/rustier/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/danielramosacosta/rustier/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.danielramosacosta.rustier.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.danielramosacosta.rustier.MyBundle
 
 class MyApplicationService {
 
